8e748120200adc0922e7d261f4823d94b83f33e9
hanifkf12 <bagiyo666@gmail.com>
2019-04-29 09:10:03 +0700

    1.5 Change Database

--- config/database.go
+++ config/database.go
@@ -6,8 +6,8 @@ import (
 )
 
 func DBInit() *gorm.DB {
-	db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/shejak-test?charset=utf8&parseTime=True&loc=Local")
-	//db, err := gorm.Open("postgres", "host=localhost port=5432 user=deploy dbname=she_jek_test password=123456 sslmode=disable")
+	//db, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/shejak-test?charset=utf8&parseTime=True&loc=Local")
+	db, err := gorm.Open("postgres", "host=localhost port=5432 user=deploy dbname=she_jek_test password=123456 sslmode=disable")
 	if err != nil {
 		panic("failed to connect to database " + err.Error())
 	}
